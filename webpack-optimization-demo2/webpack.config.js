5a2149ffb1c6fb7fc79020844cb11d678abd4563
Vivek <viveksahu.01@gmail.com>
2024-04-29 01:07:49 +0530

    minify

--- webpack-optimization-demo2/webpack.config.js
+++ webpack-optimization-demo2/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -34,5 +35,7 @@ module.exports = {
   optimization: {
     usedExports: true,
     sideEffects: true,
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   }
 };
