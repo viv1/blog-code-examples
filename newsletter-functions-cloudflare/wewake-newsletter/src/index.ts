2eb2361170fe074d5742410649ffc7c2185fd67a
Vivek <viveksahu.01@gmail.com>
2024-05-08 21:31:48 +0530

    integrate with D1

--- newsletter-functions-cloudflare/wewake-newsletter/src/index.ts
+++ newsletter-functions-cloudflare/wewake-newsletter/src/index.ts
@@ -11,13 +11,36 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	// If you set another name in wrangler.toml as the value for 'binding',
+	// replace "DB" with the variable name you defined.
+	DB: D1Database;
+  }
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+
+		const corsHeaders = {
+			'Access-Control-Allow-Headers': '*', // What headers are allowed. * is wildcard. Instead of using '*', you can specify a list of specific headers that are allowed, such as: Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Authorization.
+			'Access-Control-Allow-Methods': 'POST', // Allowed methods. Others could be GET, PUT, DELETE etc.
+			'Access-Control-Allow-Origin': '*', // This is URLs that are allowed to access the server. * is the wildcard character meaning any URL can.
+		  }
+
 		async function readRequestBody(request: Request) {
 			const contentType = request.headers.get("content-type");
 			if (contentType && contentType.includes("form")) {
 				const formData = await request.formData();
 				const email = formData.get('email')
+
+				const query = `
+				INSERT INTO NewsletterEmails (email)
+				VALUES (?)`;
+
+				const result  = await env.DB
+					.prepare(query)
+					.bind(email)
+					.run();
+				
 				console.log("email added: " + email)
 				return email;
 			} else {
@@ -25,7 +48,7 @@ export default {
 			  // like an image, or some other binary data.
 			  return null;
 			}
-		  }
+		}
 
 		if (request.method === "POST") {
 			const reqBody = await readRequestBody(request);
@@ -33,10 +56,10 @@ export default {
 				const res = {
 					email: reqBody
 				}
-				return Response.json({ status: 200 });
+				return Response.json({ status: 200 }, {headers: corsHeaders });
 			}
 		  }
 		// return empty
-		return Response.json({status: 400 });
+		return Response.json({status: 400}, {headers: corsHeaders });
 	},
 };
